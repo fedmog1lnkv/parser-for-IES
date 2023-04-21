86d5bd00545571063fba26a4c5f419e901ff7701
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-04-21 21:37:09 +0800

    Парсер для файлов BG
    Функция get_bg_pandas возвращает pandas из одного файла
    Функция get_bg_all_files_pandas возвращает pandas из всей директории
    Функции подходят к файлам по типу: "LaserPerformance20230331075358.csv"

---
+++ parser/__init__.py
@@ -0,0 +1,2 @@
+from parser.get_bg import get_bg_pandas
+from parser.get_bg import get_bg_all_files_pandas
---
+++ parser/get_bg.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+import datetime
+
+
+def open_file_in_pandas(filename):
+    return pd.read_csv(filename)
+
+
+def get_date_time(filename: str) -> datetime:
+    # LaserPerformance20230331075358.csv
+    date = filename[-18:-10]  # 20230103
+    time = filename[-10:-4]  # 073311
+
+    return datetime.datetime.strptime(date + time, "%Y%m%d%H%M%S")
+
+
+def filter_UES_BG_Sputnik(data):
+    USE_BG = data.loc[(data['NE'] == 'UES_BG') & (data['Card'] == 'XS A:SAM_16') & (data['Object'] == 'oPort 1-SFP')]
+    Sputnik = data.loc[(data['NE'] == 'Sputnik') & (data['Card'] == 'XS B:SAM_16') & (data['Object'] == 'oPort 1-SFP')]
+    return pd.concat([USE_BG, Sputnik])
+
+
+def get_bg_pandas(filename):
+    """
+    UES_BG, XS A:SAM_16, oPort 1-SFP, Tx Optical Power(dBm) >
+    Sputnik, XS B:SAM_16, oPort 1-SFP, Rx Optical Power(dBm)
+
+    Sputnik, XS B:SAM_16, oPort 1-SFP, Tx Optical Power(dBm) >
+    UES_BG, XS A:SAM_16, oPort 1-SFP, Rx Optical Power(dBm)
+
+    """
+    data = open_file_in_pandas(filename)
+    UES_BG_Sputnik = filter_UES_BG_Sputnik(data)
+    UES_BG_Sputnik.insert(0, 'Date Time', [get_date_time(filename)] * 2)
+
+    return UES_BG_Sputnik[
+        [
+            'Date Time',
+            'NE',
+            'Card',
+            'Object',
+            'Tx Optical Power(dBm)',
+            'Rx Optical Power(dBm)',
+        ]
+    ]
+
+
+def get_bg_all_files_pandas(folder_path):
+    files = sorted(os.listdir(folder_path))
+    data = get_bg_pandas(folder_path + files[0])
+
+    for filename in files[1:]:
+        filename = folder_path + filename
+        data = pd.concat([data, get_bg_pandas(filename)])
+
+    return data
+
+
+if __name__ == "__main__":
+    folder_path = "../data/BG/"
+    data = get_bg_all_files_pandas(folder_path)
+
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+
+    print(data.to_string(index=False))
