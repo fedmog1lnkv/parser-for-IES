0ee08e4c761607c606396c86b2531c27885f038c
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-05-16 23:15:01 +0800

    исправление названий

--- parser/utils.py
+++ parser/utils.py
@@ -1,20 +1,20 @@
 def rename_bg_dataframe(dataframe):
     return dataframe.rename(
-        columns={'Date Time': 'date_time', 'NE': 'in_out_cannels', 'Tx Optical Power(dBm)': 'tx_optical_power',
+        columns={'Date Time': 'date_time', 'NE': 'in_out_channels', 'Tx Optical Power(dBm)': 'tx_optical_power',
                  'Rx Optical Power(dBm)': 'rx_optical_power'})
 
 
 def rename_xdm_dataframe(dataframe):
     return dataframe.rename(
-        columns={'time': 'date_time', 'ne': 'in_out_cannels', 'Tx Optical Power(dBm)': 'tx_optical_power',
+        columns={'time': 'date_time', 'ne': 'in_out_channels', 'Tx Optical Power(dBm)': 'tx_optical_power',
                  'Rx Optical Power(dBm)': 'rx_optical_power'})
 
 def rename_172_30_26_81(dataframe):
     return dataframe.rename(
-        columns={'date_time_81': 'date_time', 'ne_81': 'in_out_cannels', 'PoutAmp_81': 'tx_optical_power',
+        columns={'date_time_81': 'date_time', 'ne_81': 'in_out_channels', 'PoutAmp_81': 'tx_optical_power',
                  'PinLn': 'rx_optical_power'})
 
 def rename_172_30_26_106(dataframe):
     return dataframe.rename(
-        columns={'date_time_106': 'date_time', 'ne_106': 'in_out_cannels', 'PoutAmp_106': 'tx_optical_power',
+        columns={'date_time_106': 'date_time', 'ne_106': 'in_out_channels', 'PoutAmp_106': 'tx_optical_power',
                  'PinRX': 'rx_optical_power'})
